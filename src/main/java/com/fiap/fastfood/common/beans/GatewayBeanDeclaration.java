f1ac0929f78e0e0f9a20b7be9743253eccf5d0c3
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-06-24 11:57:20 -0300

    feature: traz classes de produto

---
+++ src/main/java/com/fiap/fastfood/common/beans/GatewayBeanDeclaration.java
@@ -0,0 +1,17 @@
+package com.fiap.fastfood.common.beans;
+
+import com.fiap.fastfood.common.interfaces.datasources.SpringDataMongoProductRepository;
+import com.fiap.fastfood.common.interfaces.gateways.ProductGateway;
+import com.fiap.fastfood.communication.gateways.ProductGatewayImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayBeanDeclaration {
+
+    @Bean
+    public ProductGateway productGateway(SpringDataMongoProductRepository repository) {
+        return new ProductGatewayImpl(repository);
+    }
+
+}
---
+++ src/main/java/com/fiap/fastfood/common/dto/response/FullProductResponse.java
@@ -0,0 +1,22 @@
+package com.fiap.fastfood.common.dto.request;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FullProductResponse {
+
+    String id;
+    String name;
+    BigDecimal price;
+    String description;
+    String type;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
