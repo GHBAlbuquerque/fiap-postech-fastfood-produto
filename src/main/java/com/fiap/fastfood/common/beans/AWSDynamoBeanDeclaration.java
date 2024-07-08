626f4267d77a152ce3c5c56340fa014b1c3928fa
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-08 14:02:38 -0300

    feature: adiciona default credentials no bean aws

--- src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
@@ -1,5 +1,6 @@
 package com.fiap.fastfood.common.beans;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ public class AWSDynamoBeanDeclaration {
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClient.builder()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(String.valueOf(Region.US_EAST_1))
                 .build();
     }
