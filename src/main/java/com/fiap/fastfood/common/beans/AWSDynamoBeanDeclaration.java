9baebf7df14978b4f6b8d3c34d1b8d16735955df
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-09 17:38:10 -0300

    test: volta testes da controller

--- src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
@@ -12,34 +12,9 @@ public class AWSDynamoBeanDeclaration {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        /*return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
-                .build();*/
-
         return AmazonDynamoDBClient.builder()
                 .withRegion(String.valueOf(Region.US_EAST_1))
                 .build();
     }
-
-
-//    @Value("${aws.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${aws.secretKey}")
-//    private String secretKey;
-//
-//    @Value("${aws.sessionToken}")
-//    private String sessionToken;
-//
-//    @Bean
-//    public AmazonDynamoDB amazonDynamoDB() {
-//        final var awsCredentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
-//
-//        return AmazonDynamoDBClient.builder()
-//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-//                .withRegion(String.valueOf(Region.US_EAST_1))
-//                .build();
-//    }
-
 
 }
