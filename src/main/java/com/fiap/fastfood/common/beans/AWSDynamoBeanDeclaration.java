24341e55c98c01af06b0b94e06ae7365ef0f3304
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-09 16:13:19 -0300

    chore: seta profile default

--- src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
@@ -1,38 +1,45 @@
 package com.fiap.fastfood.common.beans;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class AWSDynamoBeanDeclaration {
 
-    @Value("${amazonProperties.accessKey}")
-    private String accessKey;
-
-    @Value("${amazonProperties.secretKey}")
-    private String secretKey;
-
-    @Value("${amazonProperties.sessionToken}")
-    private String sessionToken;
-
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        final var awsCredentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
+        /*return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .build();*/
 
         return AmazonDynamoDBClient.builder()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .withRegion(String.valueOf(Region.US_EAST_1))
                 .build();
     }
+
+
+//    @Value("${aws.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${aws.secretKey}")
+//    private String secretKey;
+//
+//    @Value("${aws.sessionToken}")
+//    private String sessionToken;
+//
+//    @Bean
+//    public AmazonDynamoDB amazonDynamoDB() {
+//        final var awsCredentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
+//
+//        return AmazonDynamoDBClient.builder()
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                .withRegion(String.valueOf(Region.US_EAST_1))
+//                .build();
+//    }
 
 
 }
