0987970f8e76d6caedeb270a06f07ff518544e7f
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-08-23 14:46:54 -0300

    feature: ajustes testes OWASP

--- src/main/java/com/fiap/fastfood/common/exceptions/ExceptionControllerHandler.java
+++ src/main/java/com/fiap/fastfood/common/exceptions/ExceptionControllerHandler.java
@@ -3,6 +3,9 @@ package com.fiap.fastfood.common.exceptions;
 import com.fiap.fastfood.common.exceptions.custom.CreateEntityException;
 import com.fiap.fastfood.common.exceptions.custom.EntityNotFoundException;
 import com.fiap.fastfood.common.exceptions.model.ExceptionDetails;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,8 +18,7 @@ import java.util.Date;
 @RestControllerAdvice
 public class ExceptionControllerHandler extends ResponseEntityExceptionHandler {
 
-    //TODO: ADICIONAR EXCEÇÕES CUSTOMIZADAS QUE FOREM FEITAS
-
+    private static final Logger logger = LogManager.getLogger(ExceptionControllerHandler.class);
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
     public ResponseEntity<ExceptionDetails> resourceException(EntityNotFoundException ex, WebRequest request) {
@@ -51,20 +53,22 @@ public class ExceptionControllerHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUncaughtException(Exception ex, WebRequest request) {
+        logger.error("Uncaught Exception. {}", ex.getMessage());
+        logger.error("Class: {}", ex.getClass());
+
         var status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         final var message = new ExceptionDetails(
                 "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500",
                 "Internal server error. Please contact the admin.",
-                ex.getClass().toString(),
-                ex.getMessage(),
+                "NO-CODE",
+                "Unindentified error.",
                 status.value(),
                 new Date(),
                 null);
 
-        ex.printStackTrace();
-
-        return handleExceptionInternal(ex, message, null, status, request);
+        return handleExceptionInternal(ex, message, new HttpHeaders(), status, request);
     }
+
 
 }
